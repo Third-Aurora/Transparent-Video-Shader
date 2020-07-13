30ed4b7ca9bf79b973a6d9c4b8cfc2b84597ae25
Matthew Hallberg <matthewhallberg@gmail.com>
2020-07-13 15:38:31 -0400

    trans video shader in progress

---
+++ Assets/Scripts/PlaceContent.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARSubsystems;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class PlaceContent : MonoBehaviour {
+
+    public ARRaycastManager raycastManager;
+    public GraphicRaycaster raycaster;
+
+    private void Update() {
+
+        if (Input.GetMouseButtonDown(0) && !IsClickOverUI()) {
+        
+            List<ARRaycastHit> hitPoints = new List<ARRaycastHit>();
+            raycastManager.Raycast(Input.mousePosition, hitPoints, TrackableType.Planes);
+
+            if (hitPoints.Count > 0) {
+                Pose pose = hitPoints[0].pose;
+                transform.rotation = pose.rotation;
+                transform.position = pose.position;
+            }
+        }
+    }
+
+    bool IsClickOverUI() {
+        //dont place content if pointer is over ui element
+        PointerEventData data = new PointerEventData(EventSystem.current) {
+            position = Input.mousePosition
+        };
+        List<RaycastResult> results = new List<RaycastResult>();
+        raycaster.Raycast(data, results);
+        return results.Count > 0;
+    }
+}
---
+++ Assets/Scripts/ToggleAR.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+
+public class ToggleAR : MonoBehaviour {
+
+    public ARPlaneManager planeManager;
+    public ARPointCloudManager pointCloudManager;
+
+    public void OnValueChanged(bool isOn) {
+        VisualizePlanes(isOn);
+        VisualizePoints(isOn);
+    }
+
+    void VisualizePlanes(bool active) {
+        planeManager.enabled = active;
+        foreach (ARPlane plane in planeManager.trackables) {
+            plane.gameObject.SetActive(active);
+        }
+    }
+
+    void VisualizePoints(bool active) {
+        pointCloudManager.enabled = active;
+        foreach (ARPointCloud pointCLoud in pointCloudManager.trackables) {
+            pointCLoud.gameObject.SetActive(active);
+        }
+    }
+}
